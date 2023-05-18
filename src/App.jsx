d866057d7f0026d8c996264a4a6410e2281878b3
yannikontos <unreal810@gmail.com>
2023-05-18 11:15:02 +0100

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { Box, Center, Container, Flex, VStack } from '@chakra-ui/react';
+import { Box, Center, Container, Flex, Image, Heading, Stack, Button, ButtonGroup  } from '@chakra-ui/react';
+import { SunIcon } from '@chakra-ui/icons'
 import { useState } from 'react';
 
 function App() {
@@ -6,20 +7,44 @@ function App() {
   
   return (
     <>
-      <Center h='100vh'>
-        <Container w={[200, 350, 550]}  h='70vh' borderRadius='2px' p={4} bg='blue.600'>
-          <Box>
-            <Flex flexDir='row'>
-              <header>
-                <p>hello</p>
-              </header>
-                <p>hello</p>
+      <Center h='100vh' bg=''>
+        <Container w={[300, 350, 500]}  h='85vh' borderRadius='4px' p={5} bg='blue.600'>
+            <Flex flexDir='row' bg='black' gap={4} p={4} color='white'>
+              <Image 
+                borderRadius='full'
+                boxSize='80px'
+                src='/imgs/pfp.webp/'
+              />
+              
+              <Stack direction='column'>
+                <Heading size='lg'>
+                  Yannis Kontos
+                </Heading>
+                <Heading size='md'>
+                  Green Icon Online
+                </Heading>
+              </Stack>
+            
+              <Button colorScheme='teal' size='sm' onClick={() => changeTheme}>
+                <SunIcon float='right' color='white' fontSize='xl' />
+              </Button>
+
             </Flex>
-          </Box>
         </Container> 
       </Center>
     </>
     )
+}
+
+function changeTheme() {
+  const { colorMode, toggleColorMode } = useColorMode()
+  return (
+    <header>
+      <Button onClick={toggleColorMode}>
+        Toggle {colorMode === 'light' ? 'Dark' : 'Light'}
+      </Button>
+    </header>
+  )
 }
 
 export default App
