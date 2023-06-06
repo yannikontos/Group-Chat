911b956d9bb2a3dacd72271a7e11a4fd1d25de31
Yannis Kontos <unreal810@gmail.com>
2023-06-06 12:35:56 +0100

    fixing styling issues with chat messages

--- src/components/DisplayMessage.jsx
+++ src/components/DisplayMessage.jsx
@@ -1,5 +1,5 @@
 import { collection, query, getDocs, Firestore, getFirestore, onSnapshot, doc } from 'firebase/firestore';
-import { Center, Flex, Heading, Stack, Avatar, AvatarBadge, Text, Box, Button, ButtonGroup, IconButton, FormControl, Input, Spacer, Image  } from '@chakra-ui/react';
+import { Center, Flex, Heading, Stack, Avatar, AvatarBadge, Text, Box, Button, ButtonGroup, IconButton, FormControl, Input, Spacer } from '@chakra-ui/react';
 import { initializeApp } from "firebase/app";
 import { firebaseConfig } from './FirebaseConfig';
 import { getAuth } from 'firebase/auth';
@@ -21,7 +21,7 @@ export function DisplayMessage(props) {
 
     useEffect(() => {
         const collectionRef = firebase.firestore().collection('chat-room');
-        const query2 = collectionRef.orderBy('timeSent', 'asc');
+        const query2 = collectionRef.orderBy('timeSent', 'desc');
 
         const unsubscribe = query2.onSnapshot((querySnapshot) => {
             const documents = querySnapshot.docs.map((doc) => doc.data());
@@ -40,14 +40,16 @@ export function DisplayMessage(props) {
             <>
                 <Flex height='60svh' overflowY='scroll' overflowX='hidden' direction='column-reverse' gap={{ base: '2em', md:'3.4em', lg:'1.8em'}} p={4}>
                 {items.map((item) => {
-                    const { displayName, formMessage, photoURL, timeSent, uid } = item;
+                    const { formMessage, photoURL, timeSent, uid } = item;
+                    let { displayName } = item;
                     const sentUsersUid = auth.currentUser.uid == item.uid ? true : false;
+                    sentUsersUid === true ? displayName = 'You' : displayName;
 
                     return (
-                        <Heading key={crypto.randomUUID()} width={{base: '300px', md:'500px'}} alignSelf={sentUsersUid ? 'flex-start' : 'flex-end'} backgroundColor='#5d5e602a' borderRadius={sentUsersUid ? '8px 8px 8px 0px' : '8px 8px 0px 8px'} size='md' padding={2}>
-                        <Text pb={2} ml='3em' fontSize='1.4rem' textAlign={sentUsersUid  ? 'left' : 'right'} color={randomizeColors}>{displayName}</Text>
-                        <Flex gap={3} ml={1} color='#ffffff' fontWeight='normal'  justifyContent={sentUsersUid  ? 'flex-start' : 'flex-end'}>
-                            <Image order={sentUsersUid ? 1 : 2} src={photoURL || 'https://bit.ly/broken-link'} alt={displayName} w='50px' h='50px' borderRadius='full' alignSelf='center' />
+                        <Heading key={crypto.randomUUID()} minW='200px' alignSelf={sentUsersUid ? 'flex-start' : 'flex-end'} backgroundColor='#5d5e602a' borderRadius={sentUsersUid ? '8px 8px 8px 0px' : '8px 8px 0px 8px'} size='md' padding={2}>
+                        <Text pb={3} ml='1.5em' fontSize='1.4rem' textAlign={sentUsersUid  ? 'left' : 'right'} color={randomizeColors}>{displayName}</Text>
+                        <Flex gap={3} ml={1} color='#ffffff' fontWeight='normal' textAlign={sentUsersUid ? 'left' : 'right'}  justifyContent={sentUsersUid  ? 'flex-start' : 'flex-end'}>
+                            <Avatar order={sentUsersUid ? 1 : 2} src={photoURL || ''} alt={displayName} w='50px' h='50px' borderRadius='full' alignSelf='center' />
                             <Text order={sentUsersUid ? 2 : 1}>{formMessage}</Text>
                         </Flex>
                         </Heading>
